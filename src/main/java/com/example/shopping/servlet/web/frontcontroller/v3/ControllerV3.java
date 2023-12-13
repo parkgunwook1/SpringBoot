a7e88a502eaa750a538be0fe3f3dfc29e93746ec
parkgunwook1 <pkw85428@naver.com>
2023-12-13 23:00:08 +0900

    4.스프링MVC

--- src/main/java/com/example/shopping/servlet/web/frontcontroller/v3/ControllerV3.java
+++ src/main/java/com/example/shopping/servlet/web/frontcontroller/v3/ControllerV3.java
@@ -1,5 +1,9 @@
 package com.example.shopping.servlet.web.frontcontroller.v3;
 
+import com.example.shopping.servlet.web.frontcontroller.ModelView;
+
+import java.util.Map;
+
 public interface ControllerV3 {
 
     ModelView process(Map<String, String> paramMap);
