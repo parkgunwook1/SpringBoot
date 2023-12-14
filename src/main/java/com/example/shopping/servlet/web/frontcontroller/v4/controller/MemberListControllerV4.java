4af2225d917d99066ae55fa3404d4f550fa8872b
parkgunwook1 <pkw85428@naver.com>
2023-12-14 23:14:00 +0900

    4.스프링MVC

---
+++ src/main/java/com/example/shopping/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -0,0 +1,21 @@
+package com.example.shopping.servlet.web.frontcontroller.v4.controller;
+
+import com.example.shopping.servlet.domain.member.Member;
+import com.example.shopping.servlet.domain.member.MemberRepository;
+import com.example.shopping.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV4 implements ControllerV4 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        List<Member> members = memberRepository.findAll();
+
+        model.put("members",members);
+        return "members";
+    }
+}
