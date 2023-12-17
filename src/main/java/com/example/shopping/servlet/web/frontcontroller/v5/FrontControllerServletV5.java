0fb36688e9aebf14864ce714dc3490d3fc5c068c
parkgunwook1 <pkw85428@naver.com>
2023-12-17 19:28:18 +0900

    4.스프링MVC

--- src/main/java/com/example/shopping/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/com/example/shopping/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -5,7 +5,11 @@ import com.example.shopping.servlet.web.frontcontroller.MyView;
 import com.example.shopping.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import com.example.shopping.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import com.example.shopping.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import com.example.shopping.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import com.example.shopping.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import com.example.shopping.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import com.example.shopping.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import com.example.shopping.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -26,13 +30,18 @@ public class FrontControllerServletV5 extends HttpServlet {
         initHandlerAdapters();
     }
     private void initHandlerMappingMap() {
-        handlerMappingMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
-        handlerMappingMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
-        handlerMappingMap.put("/front-controller/v3/members", new MemberListControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
 
     }
     private void initHandlerAdapters() {
         handlerAdapters.add(new ControllerV3HandlerAdapter());
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
 
     }
 
